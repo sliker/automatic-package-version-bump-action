bd47c5ddad429934525013296cb2c94358d74d48
David Velásquez <zero.blend@gmail.com>
2024-02-22 18:01:54 -0500

    fix

--- src/main.js
+++ src/main.js
@@ -51,7 +51,7 @@ async function run() {
       `git commit -m "Bump version from ${packageVersion} to ${nextVersion}"`
     )
 
-    await exec(`git push origin -u ${pullRequest.head.ref}`)
+    await exec(`git push origin`)
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
