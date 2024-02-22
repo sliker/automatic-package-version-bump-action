7c71222f7de501e32a60d788b795a57536792877
David Velásquez <zero.blend@gmail.com>
2024-02-22 18:06:40 -0500

    fix

--- dist/index.js
+++ dist/index.js
@@ -31010,7 +31010,7 @@ async function run() {
       `git commit -m "Bump version from ${packageVersion} to ${nextVersion}"`
     )
 
-    await exec('git pull')
+    await exec('git pull origin HEAD:${pullRequest.head.ref}')
     await exec(`git push origin HEAD:${pullRequest.head.ref}`)
   } catch (error) {
     // Fail the workflow run if an error occurs
